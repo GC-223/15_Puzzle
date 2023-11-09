142f7c74d55e1958f14419f7e2230914fc48b5aa
GC-223 <gc223odin@gmail.com>
2023-11-08 19:49:30 -0800

    create Tile class - Tile represents a number tile that will be used on board     can check if tile is empty     can print tile (prints blank if tile is empty)

--- 15_Puzzle/main.cpp
+++ 15_Puzzle/main.cpp
@@ -7,8 +7,61 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+// tile class
+class Tile
+{
+private:
+    // hold the number of the tile
+    int m_tileNum { } ;
+    
+public:
+    // default constructor
+    Tile() = default ;
+    
+    // explicit constructor to create tiles
+    Tile( int tileNum ) : m_tileNum( tileNum ) { }
+    
+    // overload output operator so we can print tileNum
+    friend std::ostream& operator<<( std::ostream& out, const Tile& t ) ;
+    
+    // check if tile is empty
+    // return true if empty
+    bool isEmpty() { return m_tileNum == 0 ; }
+    
+    // getters
+    int getTileNum() { return m_tileNum ; }
+    
+} ;
+
+// overload output so we can print
+std::ostream& operator<<( std::ostream& out, const Tile& t )
+{
+    
+    // if the tile is blank (0) then print a blank space
+    if ( t.m_tileNum == 0 )
+    {
+        out << '\t' ;
+    }
+    else
+    {
+        out << t.m_tileNum << '\t' ;
+    }
+    
+    return out ;
+}
+
+
+
+int main(int argc, const char * argv[]) 
+{
+    
+    Tile t1 { 5 } ;
+    Tile t2 { 12 } ;
+    Tile t3 { 0 } ;
+    Tile t4 { 1 } ;
+    
+    std::cout << t1 << t2 << t3 << t4 << '\n' ;
+    
     return 0;
 }
