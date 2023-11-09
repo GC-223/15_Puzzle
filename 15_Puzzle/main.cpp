078ca7cb9dc65a1cdb4943d3bfa8e9280d735326
GC-223 <gc223odin@gmail.com>
2023-11-08 19:28:37 -0800

    Initial Commit

---
+++ 15_Puzzle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  15_Puzzle
+//
+//  Created by Greyson Chavez on 11/8/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
