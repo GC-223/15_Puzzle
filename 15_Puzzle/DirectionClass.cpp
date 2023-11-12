56c4925434dc230fc9cc3963761d9fd1e5c6686c
GC-223 <gc223odin@gmail.com>
2023-11-12 12:15:36 -0800

    create Direction class to to handle directional inputs from user

---
+++ 15_Puzzle/DirectionClass.cpp
@@ -0,0 +1,8 @@
+//
+//  DirectionClass.cpp
+//  15_Puzzle
+//
+//  Created by Greyson Chavez on 11/12/23.
+//
+
+#include "DirectionClass.hpp"
---
+++ 15_Puzzle/DirectionClass.hpp
@@ -0,0 +1,13 @@
+//
+//  DirectionClass.hpp
+//  15_Puzzle
+//
+//  Created by Greyson Chavez on 11/12/23.
+//
+
+#ifndef DirectionClass_hpp
+#define DirectionClass_hpp
+
+#include <stdio.h>
+
+#endif /* DirectionClass_hpp */
